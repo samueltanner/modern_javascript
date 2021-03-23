88f98b18e6082793b9f6a5c1d72acf8a350c1b05
samueltanner <samuel.m.s.tanner@gmail.com>
2021-03-23 11:38:11 -0700

    adds node modules and webpack

---
+++ index.js
@@ -0,0 +1,19 @@
+var moment = require('moment');
+var mathjs = require('mathjs');
+
+console.log("Hello from JS");
+
+var datetime = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+console.log(datetime);
+
+console.log(mathjs.sqrt(9));
+
+let x = 5;
+
+console.log(x);
+
+let addTwoNumbers = (x, y) => { 
+  return x + y;              
+};                               
+console.log(addTwoNumbers(1, 99));
